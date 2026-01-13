1b0484c7d1194d63faf0215a39fd094c7ad8fc61
Shantanu675 <shantanudanej05@gmail.com>
2026-01-13 17:03:05 +0530

    remove unnecessary comments

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,31 +1,3 @@
-// import React from "react";
-// import { useDispatch } from "react-redux";
-// import authService from "../../appwrite/auth";
-// import {logout} from "../../store/authSlice";
-
-// function LogoutBtn(){
-//     const dispatch = useDispatch()
-//     const logoutHandler = () => {
-//         console.log("loged out...");
-//         authService.logout().then(() => {
-//             dispatch(logout())
-//         })
-//         window.reload(); 
-//     }
-    
-
-//     return (
-//         <button 
-//         className="inline-block px-6 py-2 duration-200 hover:text-black hover:bg-red-500 rounded-full"
-//         onClick={logoutHandler}>
-//             Logout
-//         </button>
-//     )
-// }
-
-
-// export default LogoutBtn
-
 import React from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
